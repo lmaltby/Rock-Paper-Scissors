f344047ad47a7ca9222e40c4ed3bc193ba351545
Liam Maltby <liammaltby98@gmail.com>
2023-01-27 02:39:45 -0500

    Complete first function, however has issues

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-function getComputerChoice() {
-    let random = math.floor((math.random() * 3) + 1);
-    console.log(random);
-    if (random = 1) {
-        console.log("rock");
-    } else if (random = 2) {
-        console.log("paper");
+function getComputerChoice() {  
+    let computerChoice = Math.random();
+    // log just to be sure it works
+    console.log(computerChoice);
+    //determine which response will be returned
+    if (computerChoice < 0.34) {
+        return("rock");
+    } else if (computerChoice <= 0.67) {
+        return("paper");
     } else {
-        console.log("scissors");
+        return("scissors");
     }
 }
+//call function
 getComputerChoice();
+
+// let playerSelection = prompt("Rock, Paper, or Scissors?");
+
+// function play(computerSelection, playerSelection) {
+//     if (computerSelection == rock & playerSelection == paper) {
+//         console.log("Computer wins!")
+//     }
+// }
